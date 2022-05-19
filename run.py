4ff38078a06ada1ed48922c3af71b941612bd91e
lunarwriter <sjaierasgb@gmail.com>
2022-05-19 10:32:45 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=False)
