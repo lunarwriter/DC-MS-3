bcfe7927934148065db2ed7848593f06a9251941
lunarwriter <sjaierasgb@gmail.com>
2022-05-21 08:33:43 +0000

    Changed theme, edited colors for background, nav

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,10 @@
 /*!
-* Start Bootstrap - Small Business v5.0.5 (https://startbootstrap.com/template/small-business)
+* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Highlights current date on contact page
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
